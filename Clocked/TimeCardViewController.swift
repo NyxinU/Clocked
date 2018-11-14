7caf187d0579ee6a2da40608f52c57344904c963
Nix <yiunixon@gmail.com>
2018-11-14 15:25:44 -0800

    connect add button to modal view

---
+++ Clocked/TimeCardViewController.swift
@@ -0,0 +1,31 @@
+//
+//  NewTimeCardViewController.swift
+//  Clocked
+//
+//  Created by Nix on 11/14/18.
+//  Copyright © 2018 NXN. All rights reserved.
+//
+
+import UIKit
+import CoreData
+class TimeCardViewController: UITableViewController {
+    
+    let cellId = "cellId"
+    var timecards: [Int] = [1,2,3,4,5]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return timecards.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        cell.textLabel?.text = String(timecards[indexPath.row])
+        return cell
+    }
+}
--- Clocked/ViewController.swift
+++ Clocked/ViewController.swift
@@ -11,134 +11,52 @@ import CoreData
 class ViewController: UITableViewController {
     
     let cellId = "cellId"
-    var people: [NSManagedObject] = []
+    var timecards: [NSManagedObject] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
-        
-        setupUIBarButtons()
-    }
-    
-    func setupUIBarButtons() {
-//        let rightBarButton = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(ViewController.myRightSideBarButtonItemTapped(_:)))
-//        navigationItem.rightBarButtonItem = rightBarButton
-        let addTimeButton = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(ViewController.addTime(_:)))
-        navigationItem.rightBarButtonItem = addTimeButton
-        
-        let leftBarButton = UIBarButtonItem(title: "Delete", style: .done, target: self, action: #selector(ViewController.myLeftSideBarButtonItemTapped(_:)))
-        navigationItem.leftBarButtonItem = leftBarButton
-    }
-    
-//    @objc func myRightSideBarButtonItemTapped(_ sender:UIBarButtonItem)
-//    {
-//        print("myRightSideBarButtonItemTapped")
-//        let alert = UIAlertController(title: "New Name", message: "Add a new name", preferredStyle: .alert)
-//
-//        let saveAction = UIAlertAction(title: "Save", style: .default) { [unowned self] action in
-//            guard let textField = alert.textFields?.first, let nameToSave = textField.text else {
-//                return
-//            }
-//
-//            self.save(name: nameToSave)
-//            self.tableView.reloadData()
-//        }
-//
-//        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
-//
-//        alert.addTextField()
-//        alert.addAction(saveAction)
-//        alert.addAction(cancelAction)
-//
-//        present(alert, animated: true)
-//    }
-    
-    @objc func addTime(_ sender:UIBarButtonItem) {
-        let timePicker = UIDatePicker(frame: CGRect(x: 10, y: 50, width: self.view.frame.width, height: 200))
-        
-        self.view.addSubview(timePicker)
-    }
-    
-    func save(name: String) {
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-            return
-        }
-        
-        let managedContext = appDelegate.persistentContainer.viewContext
-        
-        let entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext)!
-        
-        let person = NSManagedObject(entity: entity, insertInto: managedContext)
-        
-        person.setValue(name, forKeyPath: "name")
-        
-        do {
-            try managedContext.save()
-            people.append(person)
-        } catch let error as NSError {
-            print("Could not save. \(error), \(error.userInfo)")
-        }
-    }
-    
-    @objc func myLeftSideBarButtonItemTapped(_ sender:UIBarButtonItem)
-    {
-        print("myLeftSideBarButtonItemTapped")
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-            return
-        }
-        
-        let managedContext = appDelegate.persistentContainer.viewContext
-        
-        if let last = people.popLast() {
-            managedContext.delete(last)
-        }
-        
-        do {
-            try managedContext.save()
-        } catch let error as NSError {
-            print("Could not save. \(error), \(error.userInfo)")
-        }
-        
-        self.tableView.reloadData()
-        print(people)
+        positionAddButton()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return people.count
+        return timecards.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let person = people[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
-        
-        let name = person.value(forKeyPath: "name") as? String
-        
-        if let name = name {
-            cell.textLabel?.text = "hi\nim\n\(name)"
-        }
-        cell.textLabel?.numberOfLines = 0
         
         return cell
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    
+    let addTimeCardButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Add", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .green
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(addTimeCardButtonAction), for: .touchUpInside)
         
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-            return
-        }
+        return button
+    }()
+    
+    func positionAddButton() {
+        view.addSubview(addTimeCardButton)
         
-        let managedContext = appDelegate.persistentContainer.viewContext
-        
-        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Person")
-        
-        do {
-            people = try managedContext.fetch(fetchRequest)
-        } catch let error as NSError {
-            print("Could not fetch. \(error), \(error.userInfo)")
-        }
+        NSLayoutConstraint.activate([
+        addTimeCardButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+        addTimeCardButton.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor),
+        addTimeCardButton.heightAnchor.constraint(equalToConstant: 60),
+        addTimeCardButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
     
+    @objc func addTimeCardButtonAction(sender: UIButton!) {
+        print("Button Tapped")
+        present(TimeCardViewController(), animated: true)
+    }
 }
 
