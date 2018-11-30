2e769689f453813f007a55c7213656e7ddc1352e
Nix <yiunixon@gmail.com>
2018-11-30 15:57:33 -0800

    add section to time cards table view

--- Clocked/TimeCardsViewController.swift
+++ Clocked/TimeCardsViewController.swift
@@ -12,16 +12,19 @@ class TimeCardsViewController: UITableViewController {
     
     let cellId = "cellId"
     var timecards: [NSManagedObject] = []
+    var total: Double = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.register(TimeCardTableViewCell.self, forCellReuseIdentifier: cellId)
-        positionAddButton()
+//        layoutSubviews()
         
         let backItem = UIBarButtonItem()
         backItem.title = "Cancel"
         navigationItem.backBarButtonItem = backItem
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action:#selector(addTimeCardButtonAction(_:)))
         
         tableView.rowHeight = 60
     }
@@ -42,6 +45,7 @@ class TimeCardsViewController: UITableViewController {
         
         do {
             timecards = try managedContext.fetch(fetchRequest)
+            timecards = timecards.reversed()
             tableView.reloadData()
         } catch let error as NSError {
             print("Could not fetch. \(error), \(error.userInfo)")
@@ -49,11 +53,24 @@ class TimeCardsViewController: UITableViewController {
         print(timecards)
     }
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return timecards.count
+        if section == 0 {
+            return 1
+        } else {
+            return timecards.count
+        }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+            cell.textLabel?.text = "Total"
+            return cell
+        }
         guard let cell = tableView.dequeueReusableCell(withIdentifier: cellId) as? TimeCardTableViewCell else {
             return UITableViewCell() }
         
@@ -68,36 +85,11 @@ class TimeCardsViewController: UITableViewController {
         cell.startTimeLabel.text = startTime?.timeAsString()
         cell.endTimeLabel.text = endTime?.timeAsString()
         cell.durationLabel.text = timecard.durationAsString
-//        cell.textLabel?.text = startTime?.stringified()
 
         return cell
     }
     
-    
-    let addTimeCardButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Add", for: .normal)
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30)
-        button.setTitleColor(.white, for: .normal)
-        button.backgroundColor = .green
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.addTarget(self, action: #selector(addTimeCardButtonAction), for: .touchUpInside)
-        
-        return button
-    }()
-    
-    func positionAddButton() {
-        view.addSubview(addTimeCardButton)
-        
-        NSLayoutConstraint.activate([
-            addTimeCardButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            addTimeCardButton.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor),
-            addTimeCardButton.heightAnchor.constraint(equalToConstant: 60),
-            addTimeCardButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-        ])
-    }
-    
-    @objc func addTimeCardButtonAction(sender: UIButton!) {
+    @objc func addTimeCardButtonAction(_ sender: UIBarButtonItem) {
         navigationController?.pushViewController(TimeCardDetailsViewController(), animated: true)
     }
 
