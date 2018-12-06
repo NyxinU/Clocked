344d3e771fad9bdc0d49be2cd4e2fd8621c01fe7
Nix <yiunixon@gmail.com>
2018-12-05 20:17:14 -0800

    add pay cycle model variables

--- Clocked/PayCycle.swift
+++ Clocked/PayCycle.swift
@@ -6,4 +6,16 @@
 //  Copyright © 2018 NXN. All rights reserved.
 //
 
-//import Foundation
+import Foundation
+
+protocol PayCycleModel {
+    var startDate: Date? { get set }
+    var endDate: Date? { get set }
+    var totalHours: Double? { get }
+}
+
+class PayCycle: PayCycleModel {
+    var startDate: Date?
+    var endDate: Date?
+    var totalHours: Double?
+}
--- Clocked/PayCycleViewController.swift
+++ Clocked/PayCycleViewController.swift
@@ -1,36 +1,46 @@
-////
-////  PayCycleViewController.swift
-////  Clocked
-////
-////  Created by Nix on 12/4/18.
-////  Copyright © 2018 NXN. All rights reserved.
-////
 //
-//import UIKit
-//import CoreData
+//  PayCycleViewController.swift
+//  Clocked
 //
-//class PayCycleViewController: UITableViewController {
-//    let cellId = "cellId"
-//    var payCycles: [NSManagedObject] = []
-//    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        
-//        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
-//    }
-//    
-//    override func viewWillAppear(_ animated: Bool) {
-//        super.viewWillAppear(animated)
-//    }
-//    
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return payCycles.count
-//    }
-//    
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
-//        
-//        return cell 
-//    }
-//}
+//  Created by Nix on 12/4/18.
+//  Copyright © 2018 NXN. All rights reserved.
 //
+
+import UIKit
+import CoreData
+
+class PayCycleViewController: UITableViewController {
+    let cellId = "cellId"
+    var payCycles: [NSManagedObject] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        
+        let backItem = UIBarButtonItem()
+        backItem.title = "Pay Cycles"
+        navigationItem.backBarButtonItem = backItem
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addPayCycleButtonAction(_:)))
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return payCycles.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        
+        return cell 
+    }
+    
+    @objc func addPayCycleButtonAction(_ sender: UIBarButtonItem) {
+        
+    }
+}
+
