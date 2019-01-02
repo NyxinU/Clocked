0ba92947428bc07cbed6cbfbd55c36ef66c8d45d
Nix <yiunixon@gmail.com>
2019-01-02 14:57:23 -0800

    refactored purchase text field

--- Clocked/PurchaseTableViewCell.swift
+++ Clocked/PurchaseTableViewCell.swift
@@ -9,35 +9,47 @@
 import Foundation
 import UIKit
 
+enum PurchaseTextFieldOptions {
+    case name
+    case price
+}
+
 class PurchaseTableViewCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-        let sampleTextField =  UITextField(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
-        sampleTextField.placeholder = "Item Name"
-        sampleTextField.font = UIFont.systemFont(ofSize: 15)
-        sampleTextField.autocorrectionType = UITextAutocorrectionType.no
-        sampleTextField.keyboardType = UIKeyboardType.default
-        sampleTextField.returnKeyType = UIReturnKeyType.done
-        sampleTextField.clearButtonMode = UITextField.ViewMode.whileEditing
-        sampleTextField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
-        
-        let sampleTextField2 =  UITextField(frame: CGRect(x: 200, y: 0, width: 200, height: 40))
-        sampleTextField2.placeholder = "Price"
-        sampleTextField2.font = UIFont.systemFont(ofSize: 15)
-        sampleTextField2.autocorrectionType = UITextAutocorrectionType.no
-        sampleTextField2.keyboardType = UIKeyboardType.numberPad
-        sampleTextField2.returnKeyType = UIReturnKeyType.done
-        sampleTextField2.clearButtonMode = UITextField.ViewMode.whileEditing
-        sampleTextField2.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+        let itemNameTextField = UITextField.purchaseTextField(option: .name)
+        let priceTextField = UITextField.purchaseTextField(option: .price)
 
-        
-        addSubview(sampleTextField)
-        addSubview(sampleTextField2)
+        addSubview(itemNameTextField)
+        addSubview(priceTextField)
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension UITextField {
+    class func purchaseTextField(option: PurchaseTextFieldOptions) -> UITextField {
+        let textField = UITextField(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
+        
+        switch option {
+        case .name:
+            textField.placeholder = "Item Name"
+            textField.keyboardType = UIKeyboardType.default
+        case .price:
+            textField.frame = CGRect(x: 200, y: 0, width: 200, height: 40)
+            textField.placeholder = "$0"
+            textField.keyboardType = UIKeyboardType.numberPad
+        }
+        
+        textField.autocorrectionType = UITextAutocorrectionType.no
+        textField.returnKeyType = UIReturnKeyType.done
+        textField.clearButtonMode = UITextField.ViewMode.whileEditing
+        textField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+        
+        return textField
     }
 }
