85f4ea60a0ba98e8631d8f9a2819415ba25c6c21
Nix <yiunixon@gmail.com>
2019-01-02 18:22:18 -0800

    price text field displays proper amount

--- Clocked/PurchaseTableViewCell.swift
+++ Clocked/PurchaseTableViewCell.swift
@@ -19,8 +19,8 @@ class PurchaseTableViewCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-        let itemNameTextField = UITextField.purchaseTextField(option: .name)
-        let priceTextField = UITextField.purchaseTextField(option: .price)
+        let itemNameTextField = PurchaseTextField(option: .name)
+        let priceTextField = PriceTextField()
 
         addSubview(itemNameTextField)
         addSubview(priceTextField)
@@ -31,25 +31,79 @@ class PurchaseTableViewCell: UITableViewCell {
     }
 }
 
-extension UITextField {
-    class func purchaseTextField(option: PurchaseTextFieldOptions) -> UITextField {
-        let textField = UITextField(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
+class PurchaseTextField: UITextField, UITextFieldDelegate {
+    init(frame: CGRect = CGRect.zero, option: PurchaseTextFieldOptions) {
+        super.init(frame: frame)
         
         switch option {
         case .name:
-            textField.placeholder = "Item Name"
-            textField.keyboardType = UIKeyboardType.default
+            self.frame = CGRect(x: 20, y: 0, width: 180, height: 40)
+            self.placeholder = "Item Name"
+            self.keyboardType = UIKeyboardType.default
         case .price:
-            textField.frame = CGRect(x: 200, y: 0, width: 200, height: 40)
-            textField.placeholder = "$0"
-            textField.keyboardType = UIKeyboardType.numberPad
+            self.frame = CGRect(x: 220, y: 0, width: 180, height: 40)
+            self.placeholder = "$0"
+            self.keyboardType = UIKeyboardType.numberPad
         }
         
-        textField.autocorrectionType = UITextAutocorrectionType.no
-        textField.returnKeyType = UIReturnKeyType.done
-        textField.clearButtonMode = UITextField.ViewMode.whileEditing
-        textField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+        self.autocorrectionType = UITextAutocorrectionType.no
+        self.returnKeyType = UIReturnKeyType.done
+        self.clearButtonMode = UITextField.ViewMode.whileEditing
+        self.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+        self.delegate = self
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // called when 'return' key pressed. return NO to ignore.
+        print("TextField should return method called")
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+class PriceTextField: PurchaseTextField {
+    var amountTypedString: String?
+    init() {
+        super.init(option: .price)
         
-        return textField
+        self.amountTypedString = self.text ?? ""
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let formatter = NumberFormatter()
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        
+        if string.count > 0 {
+            amountTypedString = amountTypedString! + string
+            let decNumber = NSDecimalNumber(string: amountTypedString).multiplying(by: 0.01)
+            let newString = "$" + formatter.string(from: decNumber)!
+            textField.text = newString
+        } else {
+            amountTypedString = String(amountTypedString!.dropLast())
+            if amountTypedString!.count > 0 {
+                let decNumber = NSDecimalNumber(string: amountTypedString).multiplying(by: 0.01)
+                let newString = "$" +  formatter.string(from: decNumber)!
+                textField.text = newString
+            } else {
+                textField.text = "$0.00"
+            }
+            
+        }
+        return false
+        
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        amountTypedString = ""
+        return true
     }
 }
